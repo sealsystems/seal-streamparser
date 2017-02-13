3f1138ad2025dd97116bce2112b3a89a9230372c
Michael Scherer <michael.scherer@sealsystems.de>
2017-02-13 22:50:18 +0100

    [ci skip] Update Roboter.

--- roboter.js
+++ roboter.js
@@ -12,6 +12,9 @@ roboter.
       src: ['**/*.js', '!node_modules/**/*.js', '!examples/**', '!coverage/**', '!temp/**', '!output/**'],
       rules: '.eslintrc'
     });
+    task('universal/license', {
+      disable: true
+    });
     task('universal/test-units', {
       src: 'test/**/*Test.js'
     });
